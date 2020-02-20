e9bf1e57121d3d05e64ca3d929a4ea564e9fe05d
Dennis Valdes <dennis.valdes@anima.edu.uy>
2020-02-20 12:32:54 -0300

    Cosas

--- APM-Start/src/app/products/products.component.ts
+++ APM-Start/src/app/products/products.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
+import { isPromise } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './products.component.html'
 })
-export class ProductsComponent {
-  pageTitle: string = 'Product list';
+export class ProductsComponent implements OnInit{
+  constructor(){
+    this.filteredProducts = this.products;
+  }
+
+  pageTitle: string = 'Products list';
   showImage:boolean = false;
-  listFilter:string = 'cart';
-  products: any[] = [
+  
+  toggleImage():void {
+    this.showImage = !this.showImage;
+  }
+  
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
       "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
+      "releaseDate": "March 19, 2020",
       "description": "Leaf rake with 48-inch wooden handle.",
       "price": 19.95,
       "starRating": 3.2,
@@ -23,15 +33,35 @@ export class ProductsComponent {
       "productId": 2,
       "productName": "Garden Cart",
       "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
+      "releaseDate": "March 18, 2020",
       "description": "15 gallon capacity rolling garden cart",
       "price": 32.99,
       "starRating": 4.2,
       "imageUrl": "assets/images/garden_cart.png"
     }
   ];
+  
+  filteredProducts: IProduct[];
 
-  toggleImage():void {
-    this.showImage = !this.showImage;
+  // Filter products
+  _listFilter: string;
+
+  set listFilter(value: string){
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+  
+  ngOnInit(): void {
+    console.log('In OnInit')
   }
 }
